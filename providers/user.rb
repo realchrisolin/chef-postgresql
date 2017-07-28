7683f11aedf967bca4c40eb8a72dc14d0f0ebf03
Chris Olin <contact@chrisolin.com>
2017-07-27 23:59:43 -0400

    first attempt to workaround 'uninitialized constant Chef::Resource::PostgresqlUser' error being thrown

--- providers/user.rb
+++ providers/user.rb
@@ -51,7 +51,7 @@ action :drop do
 end
 
 def load_current_resource
-  @current_resource = Chef::Resource::PostgresqlUser.new(new_resource.name)
+  @current_resource = Chef::Resource.resource_for_node(:postgresql_user, node).new(new_resource.name)
   @current_resource.name(new_resource.name)
 
   @current_resource.exists = user_exists?
